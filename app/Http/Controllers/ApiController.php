ba99293f3ff88cb4983c079df863f0a4a222f77d
dadaxonabdullaev <dadaxona7@gmail.com>
2021-12-22 17:40:34 +0500

    5

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
     public function index()
     {
-       $posts=Http::get('https://jsonplaceholder.typicode.com/users')->json();     
-        
-        return view('jsonples',['com'=>$posts]);
+        $posts=Http::get('http://localhost:8888/users')->json();        
+        return response()->json($posts);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\PostController;
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get("api",[ApiController::class, 'index']);
 
 // Route::delete("del/{id}", [PostController::class, 'delete']);
 // Route::get('search/{name}',[PostController::class,'search']);
